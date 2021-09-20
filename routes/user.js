eea66cfca9fde11d5f75a8936f1e49f210668631
prabhuomk <prabhuomk@gmail.com>
2021-09-20 23:27:30 +0530

    small change

--- routes/user.js
+++ routes/user.js
@@ -132,7 +132,7 @@ router
         const hashedPassword=await genPassword(password);
         const updateuserpassword = await updateUser(client,id,hashedPassword);
         const deletetokens= await deletetoken(client,id);
-        response.send({message:"password updated and token got deleted"})
+        response.send({message:"password got updated"})
 
     } 
     else{
